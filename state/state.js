bc7e4b35f0e71ad9cb8a962a90316759a69ad1b2
leo Rigaudeau <leo.rigaudeau@ynov.com>
2018-03-12 18:49:47 +0100

    bonjour

--- state/state.js
+++ state/state.js
@@ -1,16 +1,36 @@
-var degat = NaN;
-var pvHero = NaN;
 
-function getlife(){
-    return pvHero-degat
-}
+var statelife ;
 
-function state (){ 
-    goodForme();
-}
-function goodForme(){
-    if(getdegat()){
-    
+function getlife(degat){
+    pvHero=100;
+    if (pvHero<=degat){
+        return 0.0
+    }else{
+        var difDeg=pvHero-degat;
+        return difDeg/pvHero  
     }
+
 }
-state(degat);
+
+function state (degat){ 
+    if(getlife(degat)>=0.8){
+        statelife="goodForme"
+    }
+    if(getlife(degat)>=0.6 && getlife()<0.8 ){
+        statelife="indemne"
+    }
+    if(getlife(degat)>=0.4 && getlife()<0.6 ){
+        statelife="blessé"
+    }
+    if(getlife(degat)>=0.20 && getlife()<0.40 ){
+        statelife="grievement Blessé"
+    }
+    if(getlife(degat)<0.20 && getlife()>0 ){
+        statelife="Zone de vie critique"
+    }
+    if(getlife(degat)<0 ){
+        statelife="Mort"
+    }
+    console.log(statelife)
+}
+
